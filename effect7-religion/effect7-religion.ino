c7e4321b3d439ba1a2ceca196a64fb2bf8c432c4
awchjimmy <awchjimmy@gmail.com>
2018-07-12 19:35:31 +0800

    Create effect7-religion.ino

---
+++ effect7-religion/effect7-religion.ino
@@ -0,0 +1,23 @@
+#include<FastLED.h>
+#define NUM_LEDS 300
+
+CRGBArray<NUM_LEDS> leds;
+
+void setup() { FastLED.addLeds<NEOPIXEL,8>(leds, NUM_LEDS); }
+
+void loop(){ 
+  static uint8_t hue;
+  for(int i = 0; i < NUM_LEDS/2; i++) {   
+    // fade everything out
+    leds.fadeToBlackBy(40);
+
+    // let's set an led value
+    leds[i] = CHSV(hue,255,255);
+    hue += 1;
+    hue %= 64;
+
+    // now, let's first 20 leds to the top 20 leds, 
+    leds(NUM_LEDS/2,NUM_LEDS-1) = leds(NUM_LEDS/2 - 1 ,0);
+    FastLED.delay(45);
+  }
+}
