23491b702440abc05469d88fcecc015ef5818a3c
awchjimmy <awchjimmy@gmail.com>
2018-11-28 07:27:46 +0800

    7 - coin flow 3 done right

--- effect7/effect7.ino
+++ effect7/effect7.ino
@@ -2,7 +2,7 @@
 
 CRGBArray<120> leds1;
 CRGBArray<145> leds2;
-CRGBArray<130> leds3;
+CRGBArray<170> leds3;
 
 const static byte COLOR_BLUE = 160; // blue
 const int d = 100;
@@ -18,7 +18,8 @@ static uint8_t dot2 = 0;
 const uint8_t cycle2 = 83;
 
 static uint8_t dot3 = 0;
-const uint8_t cycle3 = 68;
+const uint8_t cycle3 = 168;
+const uint8_t skipPoint3 = 68;
 
 
 void setup() {
@@ -26,7 +27,7 @@ void setup() {
 
   FastLED.addLeds<NEOPIXEL, 4>(leds1, 120);
   FastLED.addLeds<NEOPIXEL, 5>(leds2, 145);
-  FastLED.addLeds<NEOPIXEL, 6>(leds3, 130);
+  FastLED.addLeds<NEOPIXEL, 6>(leds3, 170);
 
   leds1 = CRGB::Black;
   leds2 = CRGB::Black;
@@ -44,7 +45,7 @@ void loop() {
   // 硬幣
   leds1(91, 120) = CHSV(COLOR_BLUE, 255, sinb);
   leds2(86, 145) = CHSV(COLOR_BLUE, 255, sinb);
-  leds3(71, 130) = CHSV(COLOR_BLUE, 255, sinb);
+  leds3(70, 129) = CHSV(COLOR_BLUE, 255, sinb);
 
   // 水流燈
   leds1(dot1 + off1, dot1 + len1 + off1) = CRGB::Blue;
@@ -58,22 +59,25 @@ void loop() {
 
 
   brightness += 4;
-//  leds1(dot1 + off1, dot1 + len1 + off1) = CRGB::Black;
-//  leds2(dot2, dot2 + 2) = CRGB::Black;
-//  leds3(dot3, dot3 + 2) = CRGB::Black;
-//  leds1.fadeToBlackBy(128);
+  //  leds1(dot1 + off1, dot1 + len1 + off1) = CRGB::Black;
+  //  leds2(dot2, dot2 + 2) = CRGB::Black;
+  //  leds3(dot3, dot3 + 2) = CRGB::Black;
+  //  leds1.fadeToBlackBy(128);
   leds1(30, 90).fadeToBlackBy(128);
   leds2(0, 85).fadeToBlackBy(128);
-  leds3(0, 70).fadeToBlackBy(128);
+  leds3(0, 170).fadeToBlackBy(128);
 
   dot1 = (dot1 + 1) % cycle1;
   dot2 = (dot2 + 1) % cycle2;
   dot3 = (dot3 + 1) % cycle3;
+  if (dot3 == skipPoint3) {
+    dot3 += 62;
+  }
 
   if (dot1 == 0) {
     len1 = 2;
     cycle1 = 60 - len1;
   }
-  
+
   delay(47);
 }
