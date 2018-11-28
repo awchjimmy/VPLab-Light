4c9072c61b7c8214160e55bcd59adf7db7734acd
awchjimmy <awchjimmy@gmail.com>
2018-11-28 09:16:28 +0800

    7 - '2' flows right

--- effect7/effect7.ino
+++ effect7/effect7.ino
@@ -1,7 +1,7 @@
 #include<FastLED.h>
 
 CRGBArray<120> leds1;
-CRGBArray<145> leds2;
+CRGBArray<240> leds2;
 CRGBArray<170> leds3;
 
 const static byte COLOR_BLUE = 160; // blue
@@ -15,7 +15,8 @@ const uint8_t off1 = 30;
 static uint8_t len1 = 2;
 
 static uint8_t dot2 = 0;
-const uint8_t cycle2 = 83;
+const uint8_t cycle2 = 238;
+const uint8_t skipPoint2 = 83;
 
 static uint8_t dot3 = 0;
 const uint8_t cycle3 = 168;
@@ -26,7 +27,7 @@ void setup() {
   Serial.begin(115200);
 
   FastLED.addLeds<NEOPIXEL, 4>(leds1, 120);
-  FastLED.addLeds<NEOPIXEL, 5>(leds2, 145);
+  FastLED.addLeds<NEOPIXEL, 5>(leds2, 240);
   FastLED.addLeds<NEOPIXEL, 6>(leds3, 170);
 
   leds1 = CRGB::Black;
@@ -44,7 +45,7 @@ void loop() {
 
   // 硬幣
   leds1(91, 120) = CHSV(COLOR_BLUE, 255, sinb);
-  leds2(86, 145) = CHSV(COLOR_BLUE, 255, sinb);
+  leds2(85, 144) = CHSV(COLOR_BLUE, 255, sinb);
   leds3(70, 129) = CHSV(COLOR_BLUE, 255, sinb);
 
   // 水流燈
@@ -64,12 +65,15 @@ void loop() {
   //  leds3(dot3, dot3 + 2) = CRGB::Black;
   //  leds1.fadeToBlackBy(128);
   leds1(30, 90).fadeToBlackBy(128);
-  leds2(0, 85).fadeToBlackBy(128);
+  leds2(0, 240).fadeToBlackBy(128);
   leds3(0, 170).fadeToBlackBy(128);
 
   dot1 = (dot1 + 1) % cycle1;
   dot2 = (dot2 + 1) % cycle2;
   dot3 = (dot3 + 1) % cycle3;
+  if (dot2 == skipPoint2) {
+    dot2 += 62;
+  }
   if (dot3 == skipPoint3) {
     dot3 += 62;
   }
