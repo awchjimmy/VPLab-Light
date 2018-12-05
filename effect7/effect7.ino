eccedf30fca8b53f4712cc85f6a6829b52af69c3
awchjimmy <awchjimmy@gmail.com>
2018-12-06 01:59:34 +0800

    7 latest bitcoin light

--- effect7/effect7.ino
+++ effect7/effect7.ino
@@ -1,8 +1,12 @@
 #include<FastLED.h>
 
-CRGBArray<120> leds1;
-CRGBArray<240> leds2;
-CRGBArray<170> leds3;
+const int total1 = 90;
+const int total2 = 165;
+const int total3 = 200;
+
+CRGBArray<total1> leds1;
+CRGBArray<total2> leds2;
+CRGBArray<total3> leds3;
 
 const static byte COLOR_BLUE = 160; // blue
 const int d = 100;
@@ -10,25 +14,23 @@ static byte brightness = 0;
 static byte sinb = 0;
 
 static uint8_t dot1 = 0;
-static uint8_t cycle1 = 58;
-const uint8_t off1 = 30;
-static uint8_t len1 = 2;
+static uint8_t cycle1 = 60 - 2;
 
 static uint8_t dot2 = 0;
-const uint8_t cycle2 = 238;
-const uint8_t skipPoint2 = 83;
+const uint8_t cycle2 = total2 - 2;
+const uint8_t skipPoint2 = 63;
 
 static uint8_t dot3 = 0;
-const uint8_t cycle3 = 168;
-const uint8_t skipPoint3 = 68;
+const uint8_t cycle3 = total3 - 2;
+const uint8_t skipPoint3 = 78;
 
 
 void setup() {
   Serial.begin(115200);
 
-  FastLED.addLeds<NEOPIXEL, 4>(leds1, 120);
-  FastLED.addLeds<NEOPIXEL, 5>(leds2, 240);
-  FastLED.addLeds<NEOPIXEL, 6>(leds3, 170);
+  FastLED.addLeds<NEOPIXEL, 4>(leds1, total1);
+  FastLED.addLeds<NEOPIXEL, 5>(leds2, total2);
+  FastLED.addLeds<NEOPIXEL, 6>(leds3, total3);
 
   leds1 = CRGB::Black;
   leds2 = CRGB::Black;
@@ -38,18 +40,15 @@ void setup() {
 }
 
 void loop() {
-  // 手機
-  // leds1(0, 29) = CRGB::Black;
-
   sinb = sin8(brightness);
 
   // 硬幣
-  leds1(91, 120) = CHSV(COLOR_BLUE, 255, sinb);
-  leds2(85, 144) = CHSV(COLOR_BLUE, 255, sinb);
-  leds3(70, 129) = CHSV(COLOR_BLUE, 255, sinb);
+  leds1(60, 89) = CHSV(COLOR_BLUE, 255, sinb);
+  leds2(65, 124) = CHSV(COLOR_BLUE, 255, sinb);
+  leds3(80, 134) = CHSV(COLOR_BLUE, 255, sinb);
 
   // 水流燈
-  leds1(dot1 + off1, dot1 + len1 + off1) = CRGB::Blue;
+  leds1(dot1, dot1 + 2) = CRGB::Blue;
   leds2(dot2, dot2 + 2) = CRGB::Blue;
   leds3(dot3, dot3 + 2) = CRGB::Blue;
 
@@ -60,27 +59,20 @@ void loop() {
 
 
   brightness += 4;
-  //  leds1(dot1 + off1, dot1 + len1 + off1) = CRGB::Black;
-  //  leds2(dot2, dot2 + 2) = CRGB::Black;
-  //  leds3(dot3, dot3 + 2) = CRGB::Black;
-  //  leds1.fadeToBlackBy(128);
-  leds1(30, 90).fadeToBlackBy(128);
-  leds2(0, 240).fadeToBlackBy(128);
-  leds3(0, 170).fadeToBlackBy(128);
+
+  leds1(0, total1).fadeToBlackBy(128);
+  leds2(0, total2).fadeToBlackBy(128);
+  leds3(0, total3).fadeToBlackBy(128);
 
   dot1 = (dot1 + 1) % cycle1;
   dot2 = (dot2 + 1) % cycle2;
   dot3 = (dot3 + 1) % cycle3;
+
   if (dot2 == skipPoint2) {
     dot2 += 62;
   }
   if (dot3 == skipPoint3) {
-    dot3 += 62;
-  }
-
-  if (dot1 == 0) {
-    len1 = 2;
-    cycle1 = 60 - len1;
+    dot3 += 57;
   }
 
   delay(47);
